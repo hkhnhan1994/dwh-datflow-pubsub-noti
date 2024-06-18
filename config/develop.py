39e39097a20fc9ea69c1c0cc2396b941caff4c93
hkhnhan1994 <hkhnhan@gmail.com>
2024-06-18 11:53:12 +0000

    structurize pipeline

--- config/develop.py
+++ config/develop.py
@@ -1,5 +1,5 @@
 beam_config={
-    "job_name": "cmd-stream-1",
+    "job_name": "cmd-stream",
     "staging_location": "gs://test_bucket_upvn/datastream-postgres/Beam-pubsub/staging",
     "temp_location": "gs://test_bucket_upvn/datastream-postgres/Beam-pubsub/temp",
     "project": "pj-bu-dw-data-sbx",
@@ -9,12 +9,28 @@ beam_config={
     "worker_region": 'europe-west1',
     "machine_type": 'n1-standard-1',
     "disk_size_gb": 10,
-    "runner": "DataflowRunner", # DirectRunner DataflowRunner
+    "runner": "DirectRunner", # DirectRunner DataflowRunner
     "setup_file": './setup.py',
     "save_main_session" : True,
     "streaming": True
 }
+pubsub_config={
+    "project": "pj-bu-dw-data-sbx",
+    "subscription": "test_sub"
+}
+cdc_ignore_fields = [
+    'stream_name',
+    'schema_key',
+    'sort_keys',
+    'source_metadata.tx_id',
+    'source_metadata.lsn',
+]
 
+bigquery_datalake ={
+    "project": "pj-bu-dw-data-sbx",
+    "region": "europe-west1",
+    "data_set": "lake_view_cmd",
+}
 pipeline_config ={
     "CMD":{
         "datalake":{
