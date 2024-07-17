ecf6fb47d3f01a1601fc8a48cea144ef8331980b
hkhnhan1994 <hkhnhan@gmail.com>
2024-07-17 08:14:53 +0000

    schema changes cogroupbykey

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -11,41 +11,37 @@ def run(beam_options):
         read_file_path = (
             p
             | read_path_from_pubsub(project=pubsub_config.get('project'),subscription=pubsub_config.get('subscription'))
+            |beam.Reshuffle()
         )
         read_data = (
-            read_file_path 
-            |read_arvo_content(cdc_ignore_fields)   
+            read_file_path
+            |read_arvo_content(cdc_ignore_fields)
+            |"bound windows arvo data" >> beam.WindowInto(FixedWindows(5)) # bound and sync with bq_schema
         )
         arvo_schema_convert = (
             read_file_path
             |arvo_schema_to_BQ_schema(cdc_ignore_fields)
+            |"bound windows arvo schema" >> beam.WindowInto(FixedWindows(5))
             )
         bq_schema = (
             arvo_schema_convert
             |read_schema_from_BQ(project=bigquery_datalake.get('project'), data_set =bigquery_datalake.get('data_set'))
             |"bound windows bq schema" >> beam.WindowInto(FixedWindows(5))
-            # |beam.Map(print)
         )
         schema_changes_handler=(
-           arvo_schema_convert
-            |"bound windows arvo schema" >> beam.WindowInto(FixedWindows(5))
-            |schema_changes(project=bigquery_datalake.get('project'), data_set =bigquery_datalake.get('data_set'),bq_schema=bq_schema)
-            |"bound windows schema changes" >> beam.WindowInto(FixedWindows(5))
+            ({'current_schema': arvo_schema_convert, 'exists_schema': bq_schema})
+            |"pair cur-exists schema">> beam.CoGroupByKey()
+            |schema_changes()
+            # |"bound windows schema changes" >> beam.WindowInto(FixedWindows(5))
+            
         )
         
         data_processing = (
-            read_data
-            |"bound windows arvo data" >> beam.WindowInto(FixedWindows(5)) # bound and sync with bq_schema
-            |mapping_data_to_schema(bq_schema=schema_changes_handler)
-            # |beam.Map(print) 
-            |"bound windows data processing" >> beam.WindowInto(FixedWindows(5)) # extra sync with write_to_BQ
-        )
-        await_schema_bound =(
-            schema_changes_handler
-            |"bound windows schema" >> beam.WindowInto(FixedWindows(5)) # extra sync with write_to_BQ
+            ({'data': read_data, 'bq_schema': schema_changes_handler})
+            |"pair data - schema" >> beam.CoGroupByKey()
+            |mapping_data_to_schema()
         )
         to_BQ = (
             data_processing
-            |beam.Reshuffle()
-            |write_to_BQ(project=bigquery_datalake.get('project'), data_set =bigquery_datalake.get('data_set'), bq_schema=await_schema_bound)
+            |write_to_BQ(project=bigquery_datalake.get('project'), data_set =bigquery_datalake.get('data_set'))
         )
