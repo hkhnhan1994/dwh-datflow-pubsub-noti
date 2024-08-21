beb6c2ba775247776cd605152cd35abcba0b8f5e
hkhnhan1994 <hkhnhan@gmail.com>
2024-08-21 04:29:43 +0000

    restore change

--- config/develop.py
+++ config/develop.py
@@ -30,10 +30,11 @@ cdc_ignore_fields = [
 bigquery_datalake ={
     "project": "pj-bu-dw-data-sbx",
     "region": "europe-west1",
-    "dataset": {
-        "cmd_test":"lake_view_cmd",
-        "paci_test":"lake_view_paci"
-        }
+    "dataset": "lake_view_cmd"
+    # "dataset": {
+    #     "cmd_test":"lake_view_cmd",
+    #     "paci_test":"lake_view_paci"
+    #     }
 }
 dead_letter = {
 "bq_channel":
